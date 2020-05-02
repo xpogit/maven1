1cbca9d8a82335f072a892a9b6aae7c53ff783c4
xpogit <azuredevopsmun@gmail.com>
2020-05-02 17:26:51 -0500

    removed .class 222

--- src/test/java/papka/temp.java
+++ src/test/java/papka/temp.java
@@ -1,20 +1,14 @@
 package papka;
 
-
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-
-public class yoyo {
-
-    Logger log = LoggerFactory.getLogger(yoyo.class);
+public class temp {
+    Logger log = LoggerFactory.getLogger(temp.class);
 
     @Test
     public void test() {
-
-        String s = "Print Marat";
-        log.info(s);
-
+        log.info("Ttttttttttttttttttttttttttuuiuosooososspsspsp");
     }
 }
